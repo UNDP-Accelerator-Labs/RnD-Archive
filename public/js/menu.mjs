59530c949d4832095e92695b1b2efeb813f2aafd
Adedapo Aderemi <adedapopaul@yahoo.com>
2025-06-20 14:31:53 +0100

    update js import path

--- public/js/menu.mjs
+++ public/js/menu.mjs
@@ -1,40 +1,38 @@
-import { d3_extended as d3 } from '/public/js/d3.extensions.mjs';
+import { d3_extended as d3 } from "./public/js/d3.extensions.mjs";
 
-var pos = 0
+var pos = 0;
 
-function handleScroll () {
-	let moving = window.pageYOffset;
-	// SET VISIBILITY
-	d3.select('nav#head').classed('moveout', pos < moving)
-	pos = moving;
+function handleScroll() {
+  let moving = window.pageYOffset;
+  // SET VISIBILITY
+  d3.select("nav#head").classed("moveout", pos < moving);
+  pos = moving;
 }
 
 export const renderMenu = async function () {
-	// LOAD MENU
-	const list = await fetch('/menu.json')
-	.then(res => res.json())
-	.catch(err => console.log(err))
+  // LOAD MENU
+  const list = await fetch("/menu.json")
+    .then((res) => res.json())
+    .catch((err) => console.log(err));
 
-	// <li><a href='/stories/'>Stories</a></li>
-	d3.select('nav#head')
-		.classed('moveout', true)
-	.select('menu')
-	.addElems('li', null, list)
-	.addElems('a')
-		.attr('href', d => `/${d}/`)
-	.html(d => d);
+  // <li><a href='/stories/'>Stories</a></li>
+  d3.select("nav#head")
+    .classed("moveout", true)
+    .select("menu")
+    .addElems("li", null, list)
+    .addElems("a")
+    .attr("href", (d) => `/${d}/`)
+    .html((d) => d);
 
-	window.addEventListener('scroll', handleScroll);
-}
+  window.addEventListener("scroll", handleScroll);
+};
 
 // const handleScroll = () => {
 //    let moving = window.pageYOffset
-   
+
 //    setVisible(position > moving);
 //    setPosition(moving)
 // };
-
-
 
 // useEffect(() => {
 //   window.addEventListener('scroll', handleScroll);
--- public/js/parsers.mjs
+++ public/js/parsers.mjs
@@ -3,66 +3,69 @@ export const tagLookup = /\[\[[\w:.\-\/\d\s…&\(\)\",]*\]\]/g;
 const typeLookup = /\[\[type:[\w:.\-\/\d\s…&\(\)\",]*\]\]/g;
 
 export const parseMetadata = function (text, metafields) {
-	if (!Array.isArray(metafields)) metafields = [metafields];
-	// RETURN ids WITH HYPERLINKS
+  if (!Array.isArray(metafields)) metafields = [metafields];
+  // RETURN ids WITH HYPERLINKS
 
-	let matches = [...text.matchAll(tagLookup)]
-	.filter(d => {
-		return metafields.some(c => d[0].replace(/[\[\]]/g, '').startsWith(c));
-	});
+  let matches = [...text.matchAll(tagLookup)].filter((d) => {
+    return metafields.some((c) => d[0].replace(/[\[\]]/g, "").startsWith(c));
+  });
 
-	// SPLICE THE text TO REMOVE THE metadata
-	matches.forEach(d => {
-		text = text.replace(d[0], '');
-	});
-	const typeMatches = [...text.matchAll(typeLookup)];
-	typeMatches.forEach(d => {
-		text = text.replace(d[0], '');
-	})
+  // SPLICE THE text TO REMOVE THE metadata
+  matches.forEach((d) => {
+    text = text.replace(d[0], "");
+  });
+  const typeMatches = [...text.matchAll(typeLookup)];
+  typeMatches.forEach((d) => {
+    text = text.replace(d[0], "");
+  });
 
-	const metadata = {};
-	matches.forEach(d => {
-		const [ key, value ] = d[0].replace(/[\[\]]/g, '').split(':');
-		// const obj = {};
-		// obj[key] = value;
-		// return obj;
-		if (metadata[key]) metadata[key].push(value);
-		else metadata[key] = [value];
-	});
-	
-	// if (metadata.length) return { ...Object.assign.apply(Object, metadata), text }; // RETURN A FLAT OBJECT
-	if (Object.keys(metadata).length) return { ...metadata, text }; // RETURN A FLAT OBJECT
-	else return { text };
-}
+  const metadata = {};
+  matches.forEach((d) => {
+    const [key, value] = d[0].replace(/[\[\]]/g, "").split(":");
+    // const obj = {};
+    // obj[key] = value;
+    // return obj;
+    if (metadata[key]) metadata[key].push(value);
+    else metadata[key] = [value];
+  });
+
+  // if (metadata.length) return { ...Object.assign.apply(Object, metadata), text }; // RETURN A FLAT OBJECT
+  if (Object.keys(metadata).length)
+    return { ...metadata, text }; // RETURN A FLAT OBJECT
+  else return { text };
+};
 export const parseTimestamps = function (text) {
-	// REGEX LOOKUP [[]]
-	// AND RETURN ids WITH HYPERLINKS
-	const lookup = /\[\[\d+[:.]?(\d+)?(:\d+)?\]\]/g;
-	return [...text.matchAll(lookup)];
-}
+  // REGEX LOOKUP [[]]
+  // AND RETURN ids WITH HYPERLINKS
+  const lookup = /\[\[\d+[:.]?(\d+)?(:\d+)?\]\]/g;
+  return [...text.matchAll(lookup)];
+};
 export const convertStringToSeconds = function (str) {
-	return str.replace(/[\[\]]+/g, '')
-		.split(/[:.]/g)
-		.reverse()
-		.map((d, i) => +d * Math.pow(60, i))
-		.reduce((accumulator, value) => accumulator + value); // CONVERT EVERYTHING TO SECONDS
-}
+  return str
+    .replace(/[\[\]]+/g, "")
+    .split(/[:.]/g)
+    .reverse()
+    .map((d, i) => +d * Math.pow(60, i))
+    .reduce((accumulator, value) => accumulator + value); // CONVERT EVERYTHING TO SECONDS
+};
 export const parseIntervals = function (text, audioDuration) {
-	let timestamps = parseTimestamps(text);
-	timestamps = timestamps.map(d => {
-		return convertStringToSeconds(d[0]);
-	})
-	timestamps = zip(timestamps, timestamps.slice(1)).flat().filter(d => d !== undefined && d < audioDuration);
-	timestamps.push(audioDuration);
-	return chunk(timestamps, 2);
+  let timestamps = parseTimestamps(text);
+  timestamps = timestamps.map((d) => {
+    return convertStringToSeconds(d[0]);
+  });
+  timestamps = zip(timestamps, timestamps.slice(1))
+    .flat()
+    .filter((d) => d !== undefined && d < audioDuration);
+  timestamps.push(audioDuration);
+  return chunk(timestamps, 2);
+};
+function chunk(arr, size) {
+  const groups = [];
+  for (let i = 0; i < arr.length; i += size) {
+    groups.push(arr.slice(i, i + size));
+  }
+  return groups;
 }
-function chunk (arr, size) {
-	const groups = [];
-	for (let i = 0; i < arr.length; i += size) {
-		groups.push(arr.slice(i, i + size));
-	}
-	return groups;
-};
-function zip (arr1, arr2) {
+function zip(arr1, arr2) {
   return arr1.map((d, i) => [d, arr2[i]]);
-};
+}
